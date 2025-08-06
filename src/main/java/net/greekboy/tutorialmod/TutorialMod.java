eef958c526d2a67f862c7e41ba086cefdfdf8283
NikolaosSarris <nsarris04@gmail.com>
2025-08-05 23:56:40 -0400

    Added a sapphire to the game.

--- src/main/java/net/greekboy/tutorialmod/TutorialMod.java
+++ src/main/java/net/greekboy/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.greekboy.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.greekboy.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -28,6 +29,8 @@ public class TutorialMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -46,7 +49,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
