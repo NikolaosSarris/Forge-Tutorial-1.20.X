10984ca65e2cf98cffc5a06f0bbfbbf3e48d8318
NikolaosSarris <nsarris04@gmail.com>
2025-08-07 19:02:16 -0400

    Added loot tables and multiples ore variations for the sapphire with their own unique textures.

--- src/main/java/net/greekboy/greekboymod/item/ModCreativeModeTabs.java
+++ src/main/java/net/greekboy/greekboymod/item/ModCreativeModeTabs.java
@@ -24,10 +24,17 @@ public class ModCreativeModeTabs
                         //Gets the custom item / block to display in the tab
                         output.accept(ModItems.SAPPHIRE.get());
                         output.accept(ModItems.RAW_SAPPHIRE.get());
-                        output.accept(ModItems.HOLY_CROSS.get());
 
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.DEEPLSATE_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                        output.accept(ModItems.HOLY_CROSS.get());
+
                     })
                     .build());
 
