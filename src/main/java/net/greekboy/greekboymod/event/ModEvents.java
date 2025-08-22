722c762565b8fa07f529557eaca6d9bcd390121e
NikolaosSarris <nsarris04@gmail.com>
2025-08-22 14:05:22 -0400

    Added custom villager trades to the game.

---
+++ src/main/java/net/greekboy/greekboymod/event/ModEvents.java
@@ -0,0 +1,76 @@
+package net.greekboy.greekboymod.event;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.greekboy.greekboymod.TutorialMod;
+import net.greekboy.greekboymod.item.ModItems;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
+public class ModEvents
+{
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event)
+    {
+        //Add trade to only the farmer villager
+        if (event.getType() == VillagerProfession.FARMER)
+        {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            //Adding a custom trade
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    new ItemStack(ModItems.STRAWBERRY.get(), 12),
+                            10, 8, 0.02f));
+
+            //Adding a custom trade
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    new ItemStack(ModItems.CORN.get(), 6),
+                    5, 8, 0.035f));
+        }
+
+        if (event.getType() == VillagerProfession.LIBRARIAN)
+        {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack enchantedBook = EnchantedBookItem.createForEnchantment(new EnchantmentInstance(Enchantments.THORNS, 2));
+
+            //Adding a custom trade
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 32),
+                    enchantedBook,
+                    10, 8, 0.02f));
+        }
+    }
+
+    @SubscribeEvent
+    public static void addCustomWanderingTrades(WandererTradesEvent event)
+    {
+        //Lists for the generic and rare trades
+        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+        List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
+
+        //Adding custom trades
+        genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 12),
+                new ItemStack(ModItems.SAPPHIRE_BOOTS.get(), 1),
+                10,2,0.2f));
+
+        rareTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 42),
+                new ItemStack(ModItems.METAL_DETECTOR.get(), 1),
+                10,2,0.2f));
+    }
+}
